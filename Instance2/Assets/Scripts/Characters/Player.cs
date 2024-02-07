5ac5ae975547cfaa619ed2c23275f02521f8f0c0
xinom4966 <sd.dessay@gmail.com>
2024-02-07 15:34:40 +0100

    Added Inventory
    Edited:
    -Player Script
    Created:
    -Inventary Scene

--- Instance2/Assets/Scripts/Characters/Player.cs
+++ Instance2/Assets/Scripts/Characters/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEditor.Timeline.TimelinePlaybackControls;
 
-public class Player : MonoBehaviour
+public class Player : Entity
 {
     [SerializeField] private int _perkLimit;
     private List<CardBaseGrave> _perkList;
@@ -16,6 +16,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float _cellSize;
     [SerializeField] private Ressources _ressources;
     [SerializeField] private Transform _playerPos;
+    [SerializeField] private List<CardBaseGrave> _inventoryPlayer1 = new List<CardBaseGrave>();
+    [SerializeField] private List<CardBaseGrave> _inventoryPlayer2 = new List<CardBaseGrave>();
+    [SerializeField] private bool _haveTreasure;
+    public List<CardBaseGrave> InventoryPlayer1
+    {
+        get { return _inventoryPlayer1; }
+    }
+    public List<CardBaseGrave> InventoryPlayer2
+    {
+        get { return _inventoryPlayer2; }
+    }
 
     void Start()
     {
@@ -42,6 +53,10 @@ public class Player : MonoBehaviour
 
     public void NavigateAssets(InputAction.CallbackContext context)
     {
+        if (!_isTurn) 
+        { 
+            return; 
+        }
         if (_selectedPerk + context.ReadValue<Vector2>().x.ConvertTo<int>() <= _perkList.Count && _selectedPerk + context.ReadValue<Vector2>().x.ConvertTo<int>() >= 1)
         {
             if (!_perkList[_selectedPerk + context.ReadValue<Vector2>().x.ConvertTo<int>()].IsUnityNull())
@@ -57,42 +72,35 @@ public class Player : MonoBehaviour
         
     }*/
 
-    public void SetCanOpenTomb(bool value)
-    {
-        _canOpenTombs = value;
-    }
-
-    public void SetAssetLimit(int value)
-    {
-        _perkLimit = value;
-    }
-
     public void GetDirections(Vector2 direction)
     {
-        if (_myState == PlayerState.idle) 
-        { 
-        switch (direction.y)
+        if (_isTurn)
         {
-            case 1:
-                _destination = _playerPos.position + Vector3.up * _cellSize;
-                break;
-
-            case -1:
-                _destination = _playerPos.position + Vector3.down * _cellSize;
-                break;
-        }
-
-            switch (direction.x)
+            if (_myState == PlayerState.idle)
             {
-                case 1:
-                    _destination = _playerPos.position + Vector3.right * _cellSize;
-                    break;
+                switch (direction.y)
+                {
+                    case 1:
+                        _destination = _playerPos.position + Vector3.up * _cellSize;
+                        break;
 
-                case -1:
-                    _destination = _playerPos.position + Vector3.left * _cellSize;
-                    break;
+                    case -1:
+                        _destination = _playerPos.position + Vector3.down * _cellSize;
+                        break;
+                }
+
+                switch (direction.x)
+                {
+                    case 1:
+                        _destination = _playerPos.position + Vector3.right * _cellSize;
+                        break;
+
+                    case -1:
+                        _destination = _playerPos.position + Vector3.left * _cellSize;
+                        break;
+                }
+                _myState = PlayerState.moving;
             }
-            _myState = PlayerState.moving;
         }
     }
 
@@ -100,6 +108,42 @@ public class Player : MonoBehaviour
     {
 
     }*/
+
+    public void AddToInventory()
+    {
+        //_inventoryPlayer1.Add(_grave.Interact());
+    }
+
+    public bool GetObject()
+    {
+        for (int i = 0; i < _inventoryPlayer1.Count; i++)
+        {
+            if (_inventoryPlayer1[i].CardType == CardType.Key)
+            {
+                _inventoryPlayer1.RemoveAt(i);
+                return true;
+            }
+            else
+            {
+                Debug.Log("You can't open the graveStone, get a Key");
+            }
+        }
+        return false;
+    }
+
+    public void SetBool()
+    {
+        /*if (_stone.Interact() == false)
+        {
+            _haveTreasure = false;
+            Debug.Log("Sorry Wrong GraveStone");
+        }
+        else if (_stone.Interact() == true)
+        {
+            _haveTreasure = true;
+            Debug.Log("Get The Treasure");
+        }*/
+    }
 }
 
 public enum PlayerState
