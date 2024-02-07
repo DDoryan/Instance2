094b6e68e6979276fee54a938c8e415ab9e99360
kalderlunt <kalderlunt@gmail.com>
2024-02-07 11:06:20 +0100

    Modifiy RollDice
    Modifier script :
     - RollSimple.cs
    
    new script :
     - CameraWindowDice.cs

--- Instance2/Assets/Scripts/RollDice/ReurnRandom/DiceSimple.cs
+++ Instance2/Assets/Scripts/RollDice/ReurnRandom/DiceSimple.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiceSimple : MonoBehaviour
 {
     private int _diceNumber;
+    [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private string _textBeforeValue = "Dice Number : ";
 
-    public void RollDice()
+    private void Update()
     {
-        _diceNumber = Random.Range(1, 20);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice(6);
+        }
+        
+        if (_diceNumber != 0)
+        {
+            _scoreText.text = _textBeforeValue + " " + _diceNumber;
+        }
+    }
+
+    public void RollDice(int numberOfSide)
+    {
+        _diceNumber = Random.Range(1, numberOfSide);
         //return 0;
     }
 
